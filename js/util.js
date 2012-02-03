c28f4fd3ae74a94e50a55aeec28e7cf516da7812
spemoon <lidian.sw@gmail.com>
2012-02-03 19:12:58 +0800

    add js

---
+++ js/util.js
@@ -0,0 +1,149 @@
+(function() {
+
+    if(!window.LD){
+        window['LD'] = {};
+    }
+
+    var helper = {
+        isCompatible : function(other) {
+            if(other === false || !Array.prototype.push || !Object.hasOwnProperty || !document.createElement || !document.getElementsByTagName){
+                return false;
+            }
+            return true;
+        },
+        $ : function() {
+            var elements = [];
+            for(var i = 0,len = arguments.length; i < len; i++){
+                var element = arguments[i];
+                console.log(element);
+
+                if(element == 'string'){
+                    element = document.getElementById(element);
+                }
+
+                if(len == 1){
+                    return element;
+                }
+
+                elements.push(element);
+            }
+            return elements;
+        },
+        addEvent :  function(node, type, listener) {
+            if(!helper.isCompatible()){
+                return false;
+            }
+            console.log(helper.$(node));
+            console.log(node);
+            if(!(node = helper.$(node))){
+                return false;
+            }
+
+            if( node.addEventListener ){
+                node.addEventListener(type, listener, false);
+                return true;
+            }else if( node.attachEvent ){
+                node['e' + type + listener] = listener;
+                node[type + listener] = function() {
+                    node['e' + type + listener](window.event);
+                }
+                node.attachEvent( 'on' + type, node[type + listener]);
+                return true;
+            }
+            return false;
+        },
+        removeEvent : function(node, type, listener) {
+            if(!(node = helper.$(node))){
+                return false;
+            }
+
+            if(node.removeEventListener){
+                node.removeEventListener(type, listener, false);
+                return true;
+            }else if(node.detachEvent){
+                node.detachEvent( 'on' + type, node[type + listener]);
+                node[type + listener] = null;
+                return true;
+            }
+            return false;
+        },
+        getElementByClassName : function(className, tag, parent) {
+            parent = parent || document;
+            var allTags = (tag == '*' && parent.all) ? parent.all : parent.getElementsByTagName(tag);
+            var matchingElements = [];
+
+            className = className.replace(/\-/g, '\\-');
+            var regex = new RegExp('(^|\\s)' + className + '\\s|$');
+
+            var element;
+            for(var i = 0,len = allTags.length; i < len; i++){
+                element = allTags[i];
+                if(regex.test(element.className)){
+                    matchingElements.push(element);
+                }
+            }
+            return matchingElements;
+        },
+        toggleDisplay : function(node, value) {
+            if(!(node = helper.$(node))){
+                return false;
+            }
+
+            if(node.style.display != 'none'){
+                node.style.display = 'none';
+            }else{
+                node.style.display = value || '';
+            }
+            return ture;
+        },
+        insertAfter : function(node, referenceNode) {
+            if(!(node = helper.$(node))){
+                return false;
+            }
+
+            if(!(referenceNode = helper.$(node))){
+                return false;
+            }
+
+            return referenceNode.insertBefore(node, referenceNode.nextSibling);
+        },
+        removeChildren : function(parent) {
+            if(!(parent = helper.$(parent))){
+                return false;
+            }
+
+            while(parent.firstChild){
+                parent.firstChild.parentNode.removeChild(parent.firstChild);
+            }
+
+            return parent;
+        },
+        prependChild : function(parent, newChild) {
+            if(!(parent = helper.$(parent))){
+                return false;
+            }
+            if(!(newChild = helper.$(newChild))){
+                return false;
+            }
+
+            if(parent.firstChild){
+                parent.insertBefore(newChild, parent.firstChild);
+            }else{
+                parent.appendChild(newChild);
+            }
+
+            return parent;
+        }
+    }
+
+    window['LD']['isCompatible'] = helper.isCompatible;
+    window['LD']['$'] = helper.$;
+    window['LD']['addEvent'] = helper.addEvent;
+    window['LD']['removeEvent'] = helper.removeEvent;
+    window['LD']['getElementByClassName'] = helper.getElementByClassName;
+    window['LD']['toggleDisplay'] = helper.toggleDisplay;
+    window['LD']['insertAfter'] = helper.insertAfter;
+    window['LD']['removeChildren'] = helper.removeChildren;
+    window['LD']['prependChild'] = helper.prependChild;
+
+})();
